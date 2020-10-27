2c187a17905202d2e11a955ac519a8f51ec0b61c
Hi-ro-ki <kohibreakemeeting@gmail.com>
2020-10-27 19:50:49 +0900

    "b,u出力"

--- #MANTRA.py
+++ #MANTRA.py
@@ -1,29 +1,33 @@
-#MANTRA
-"""plaintext=0x0000000000000000 #64bit
-pL=(plaintext>>32)&0xffffffff #32bit
-pR=plaintext&0xffffffff #32bit
-"""
 
-"""
-### key schedule
-key=0x00000000000000000000000000000000
-rk1=[]
-rk2=[]
-s=[0x2,0x5,0xd,0xa,0xf,0x3,0x4,0x9,0xb,0x0,0x6,0xc,0x8,0xe,0x1,0x7]
-for i in range(round):
-    rkey1=(key>>16)&0xffff #16bits
-    rk1.append(rkey1)
-    rkey2=key&0xffff #16bits
-    rk2.append(rkey2)
-    key=((key<<27)&0xffffffffffffffffffffffffffffffff)^(key>>101)  ###27shift
-    a=key&0xf
-    b=s[a]
-    c=(key>>4)&0xf
-    d=s[c]
-    e=key^(i<<66)
-    f=(e>>8)<<8 ##sboxの結果を反映させるために8bit0埋めに
-    key=(f^b)^(d<<4)##sboxの結果反映
-"""
+import random
+def main():
+    print("入力Integral特性：aaaacc~c")                                        ####　変更
+    ROUND = int( input("何段分のintegral特性を求めますか？　：") )
+    ATEMPT = 100000 #段鍵、平文のランダム設定の回数
+
+    #出力部のIntegral特性　0で初期化
+    output_integral=0x0000000000000000
+    #print(lici(0x0000000000000000,0xffffffffffffffffffffffffffffffff,31))
+    for a in range(ATEMPT):
+        #平文をランダムに設定 64bits
+        plain_text = random.randint(0x0000000000000000,0xffffffffffffffff)
+        #段鍵をランダムに設定 128bits
+        key = random.randint(0x00000000000000000000000000000000,0xffffffffffffffffffffffffffffffff)
+
+        xor_sum=0x0000000000000000
+        
+        #Integral特性 aacccc の計算
+        for delta_p in range(2**4):                                            ### 変更
+            cryptgram = mantra(plain_text ^ (delta_p<<60) ,key,ROUND)
+            #print( "0b"+format(cryptgram, '06b'))#暗号文を0埋め6桁2進数で出力
+            xor_sum ^= cryptgram
+        
+        output_integral = output_integral | xor_sum #output_integralにxor総和をOR演算して代入　最後まで0が続いたビットがxor総和０ってこと
+    
+    output_integral = format(output_integral, '064b')
+    print(ROUND,"段: 出力Integral特性：" + output_integral.replace('0', 'b').replace('1', 'u'))
+
+#####暗号器####
 #sbox
 def sbox(p):
 
@@ -57,7 +61,7 @@ def func2(pt3,pt2,i,rk2):
     return pf
 
 ##全体##
-def encryption(plaintext,key,round):
+def mantra(plaintext,key,round):
 
     pl=(plaintext>>32)&0xffffffff #32bit
     pr=plaintext&0xffffffff #32bit
@@ -97,5 +101,11 @@ def encryption(plaintext,key,round):
     ct=(pl<<32)^pr
     return ct
 
-c = encryption(0x0000000000000000,0x0123456789abcdef0123456789abcdef,32)
-print("出力:",hex(c))
+#c = mantra(0x0000000000000000,0x0123456789abcdef0123456789abcdef,32)
+#print("出力:",hex(c))
+
+
+
+
+if __name__=="__main__":
+    main()
